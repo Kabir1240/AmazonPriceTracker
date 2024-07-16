e3cdbc73e4c0084f69ed8332b00ab1939659c2c9
Kabir Kashif <kashif.kabir60@gmail.com>
2024-07-16 19:28:05 +0500

    added main functionality, for dummy data

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import requests
 import os
 import smtplib as smtp
 from bs4 import BeautifulSoup
-from dotenv import dot_env
+from dotenv import load_dotenv
 
 
-FROM_EMAIL = os.environ.get("email")
-EMAIL_PASSWORD = os.environ.get("password")
+load_dotenv()
+FROM_EMAIL = os.environ.get("FROM_EMAIL")
+EMAIL_PASSWORD = os.environ.get("PASSWORD")
+TO_EMAIL = os.environ.get("TO_EMAIL")
 
-def send_email(self, from_email, from_pass, to_email, subject, message_body) -> None:
+
+def send_email(from_email, from_pass, to_email, subject, message_body) -> None:
     """
     sends email from one account to another
     :return: None
@@ -18,7 +21,7 @@ def send_email(self, from_email, from_pass, to_email, subject, message_body) -> 
         connection.login(user=from_email, password=from_pass)
         connection.sendmail(
             from_addr=from_email,
-            to_addr=to_email,
+            to_addrs=to_email,
             msg=f"{subject}\n\n{message_body}")
 
 
@@ -31,4 +34,8 @@ price = float(soup.find(name="span", class_="a-price-whole").text + \
         soup.find(name="span", class_="a-price-fraction").text)
 
 if price <= 100:
-    print(FROM_EMAIL, EMAIL_PASSWORD)
+    item = soup.find(name="span", id="productTitle").text.replace("\xe9", "")
+    subject = "AMAZON SALE!"
+    message_body = f"{item} is now {price}\n{url}"
+    print(message_body)
+    send_email(FROM_EMAIL, EMAIL_PASSWORD, TO_EMAIL, subject, message_body)
